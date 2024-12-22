849eaf3761c68b39a67382179c8ffb2c63e2714e
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2024-12-22 21:39:59 +0600

    add messenger

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Beer from "./Components/Beer/Beer";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const App = () => {
   return (
     <div className="container">
+       <MessengerCustomerChat
+    pageId="100202745252345"
+    appId="564263663154134"
+    htmlRef="<REF_STRING>"
+  />,
       <Beer></Beer>
     </div>
   );
--- src/Components/Beer/Beer.js
+++ src/Components/Beer/Beer.js
@@ -1,27 +1,46 @@
-import React, { useEffect, useState } from "react";
-import BeerCard from "../BeerCard/BeerCard";
+import React, { useState } from "react";
 import "./Beer.css";
 import PasswordGenerator from "../PasswordGenerator/PasswordGenerator";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const Beer = () => {
   const [query, setQuery] = useState("");
   console.log(query);
-  const [beers, setBeers] = useState([]);
-  useEffect(() => {
-    const url = `https://api.punkapi.com/v2/beers`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setBeers(data);
-      });
-  }, [query]);
-  const handleSearch = () => {
-    const filteredProducts = beers.filter((item) =>
-      item.name.toLowerCase().includes(query.toLowerCase())
-    );
-    setBeers(filteredProducts);
-  };
+  // const [beers, setBeers] = useState([]);
+
+  // Fetch Function
+  // const fetchBeers = async () => {
+  //   const { data } = await axios.get("https://api.punkapi.com/v2/beers");
+  //   return data;
+  // };
+  // TanStack Query to fetch data
+  const {
+    data,
+    // isLoading, isError, error, isSuccess
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const { data } = await axios.get("https://dummyjson.com/products");
+      console.log(data?.products);
+      return data;
+    },
+    onSuccess: () => {
+      console.log("Data fetched successfully");
+    },
+    onError: (error) => {
+      console.error("Error fetching data:", error.message);
+    },
+  });
+
+  console.log(data);
+
+  // const handleSearch = () => {
+  //   const filteredProducts = beers.filter((item) =>
+  //     item.name.toLowerCase().includes(query.toLowerCase())
+  //   );
+  //   setBeers(filteredProducts);
+  // };
 
   return (
     <div>
@@ -29,7 +48,7 @@ const Beer = () => {
         <PasswordGenerator />
         <div className="d-flex justify-content-between align-items-center gap-5">
           <p className="text-danger fw-bold">
-            Show all products: {beers.length}
+            {/* Show all products: {products?.length} */}
           </p>
           <div className="input-group mb-3 w-50">
             <input
@@ -39,14 +58,17 @@ const Beer = () => {
               value={query}
               onChange={(e) => setQuery(e.target.value)}
             />
-            <button className="btn btn-danger" onClick={handleSearch}>
+            <button
+              className="btn btn-danger"
+              // onClick={handleSearch}
+            >
               Search
             </button>
           </div>
         </div>
-        <div className="card_container ">
-          {beers?.slice(0, 12)?.map((item) => (
-            <BeerCard item={item} key={item.id}></BeerCard>
+        <div className="card_container">
+          {data?.products?.map((item) => (
+            <p>{item?.title}</p>
           ))}
         </div>
       </div>
